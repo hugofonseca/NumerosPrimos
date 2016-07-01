d21cdda773e05cf36fbc5b35150ef7f982832c86
hugofonseca <hugo.mig.28@gmail.com>
2016-07-01 10:29:38 -0300

    1oCOMMIT_ok
    funcionalidades a partir de listas e condicionais

---
+++ NumerosPrimos/NumerosPrimos/Primos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPrimos
+{
+    class Primos
+    {
+        public static void primos(int range)
+        {
+            List<int> numeros = new List<int>();
+            List<int> primos = new List<int>();
+            for(int i = 2; i <= range; i++)
+            {
+                numeros.Add(i);
+                primos.Add(i);
+            }
+
+            foreach (int i in numeros)
+            {
+                foreach (int j in numeros)
+                {
+                    if(j % i == 0 && i != j)
+                    {
+                        primos.Remove(j);
+                    }
+                }
+            }
+            foreach (int j in primos)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
